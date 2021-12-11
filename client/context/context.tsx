9f4808a99ff6e1579388fd6c969fd92a8cff2976
Chris Barber-Riley <chris.barberriley@gmail.com>
2021-12-11 21:00:22 +0000

    Setup darkmode

---
+++ client/context/context.tsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const ThemeContext = createContext<any>({} as any);
+
+export default ThemeContext;
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,33 +1,32 @@
-import { PaletteMode } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
 import { useMemo, useState } from 'react';
+import ThemeContext from '../context/context';
 import getDesignTokens from '../src/theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [mode, setMode] = useState<PaletteMode>('dark');
-  const colorMode = useMemo(
-    () => ({
-      // The dark mode switch would invoke this method
-      toggleColorMode: () => {
-        setMode((prevMode: PaletteMode) =>
-          prevMode === 'light' ? 'dark' : 'light',
-        );
-      },
-    }),
-    [],
+  const [mode, setMode] = useState(false);
+
+  const theme = useMemo(
+    () => createTheme(getDesignTokens(mode ? 'dark' : 'light')),
+    [mode],
   );
 
-  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
-
   return (
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ThemeContext.Provider
+      value={{
+        mode,
+        setMode,
+      }}
+    >
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ThemeContext.Provider>
   );
 }
 
