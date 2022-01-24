b5706c849e1c86b1f904b71e1681965981656feb
Chris Barber-Riley <chris.barberriley@gmail.com>
2022-01-24 15:27:43 +0000

    Hard coding env vars as debug check

--- src/lib/apollo-client.tsx
+++ src/lib/apollo-client.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.WORDPRESS_GRAPHQL_ENDPOINT,
+  uri: 'http://18.132.52.127/graphql',
   cache: new InMemoryCache(),
 });
 
