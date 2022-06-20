2e1c2ba2707be776c46ad2578be8150978a7dc45
Chris Barber-Riley <chris.barberriley@gmail.com>
2022-06-20 17:35:16 +0100

    Add: template func for deploy

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,11 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Nav = () => {
   const navItems = [
-    { name: 'Home', link: '/' },
-    { name: 'Uses', link: '/uses' },
-    { name: 'Reading', link: '/books' },
+    { name: "Home", link: "/" },
+    { name: "Uses", link: "/uses" },
+    { name: "Reading", link: "/books" },
+    // { name: "Templates", link: "/templates" },
     // { name: 'Tech stack', link: '/tech' },
     // { name: 'About', link: '/about' },
     // { name: 'Contact', link: '/contact' },
@@ -12,11 +13,11 @@ const Nav = () => {
 
   return (
     <nav>
-      <ul className='flex'>
+      <ul className="flex">
         {navItems.map(({ name, link }) => (
-          <li className='p-4' key={name}>
+          <li className="p-4" key={name}>
             <Link href={link}>
-              <a className='text-metal-500 hover:text-white'>{name}</a>
+              <a className="text-metal-500 hover:text-white">{name}</a>
             </Link>
           </li>
         ))}
---
+++ pages/templates.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Templates: NextPage = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default Templates;
